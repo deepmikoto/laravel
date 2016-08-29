191cb381f97d029ebb157c2680c55c57a18c99df
alexandru.vasileniuc <deepmikoto@gmail.com>
2016-08-29 22:42:06 +0300

    blade template 404 page & test service provider

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>404</title>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700' rel='stylesheet' type='text/css'>
+    <style>body { font-family: 'Roboto Condensed', sans-serif; }.not-found { position: absolute; left: 50%; top: 50%; margin-left: -285px; margin-top: -190px; }</style>
+</head>
+<body>
+<h2 style="text-align: center; margin-top: 100px">You've ripped a hole in the fabric of the internet.</h2>
+
+<img src="{{ URL::asset('images/404.jpg') }}" alt="404" class="not-found"/>
+
+<h5 style="text-align: center"><a href="{{ '/' }}">Back to homepage</a></h5>
+</body>
+</html>
